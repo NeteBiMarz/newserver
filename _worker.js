888d37196b8000b3678e1411fd232c8c845a5235
NeteBiMarz <155294240+NeteBiMarz@users.noreply.github.com>
2024-08-05 22:56:40 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'c345a325-22de-451a-83c5-70557ac93931';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
